1882e2e1d138a355407ce6224f06afdf923eed59
lokesh-ns <lokeshns7476loki@gmail.com>
2023-11-30 14:37:00 +0530

    modified local file creation

--- 1. Terraform-Study-Documents/1. terraform-file_creation/main.tf
+++ 1. Terraform-Study-Documents/1. terraform-file_creation/main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "sample_files" {
-    filename = "C:/Users/HP/Desktop/Terraform-1/local-file-creation/sample_file1.txt"
+    filename = "./sample_text_file.txt"
     content = "hey there, welcome to terraform course"
 }
